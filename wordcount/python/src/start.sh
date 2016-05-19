f524d2d5b91a16b2849a499b57e3b31ff8f70ea8
otajisan <taji.taji.otajisan@gmail.com>
2016-05-19 16:34:29 +0900

    modified

--- wordcount/python/src/start.sh
+++ wordcount/python/src/start.sh
@@ -6,10 +6,16 @@ rm -fR ./output
 # Hadoopコマンドの実行
 /usr/local/bin/hadoop \
     jar /usr/local/Cellar/hadoop/2.7.2/libexec/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar \
+    -Dmapred.reduce.tasks=3 \
+    -Dmapred.map.output.compression.codec=org.apache.hadoop.io.compress.DefaultCodec \
+    -Dmapred.output.compression.codec=org.apache.hadoop.io.compress.GzipCodec \
+    -Dmapred.output.compress=true \
+    -Dmapred.compress.map.output=true \
     -mapper WordCountMapperScript.py \
     -reducer WordCountReducerScript.py \
     -file WordCountMapperScript.py WordCountReducerScript.py \
     -input ./input \
     -output ./output
 
-cat ./output/*
+# 結果出力
+gzcat ./output/*.gz
